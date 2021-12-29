268356ea9bb00aab6d23febd6eac0080e1589105
itsmaream <pro.maream.18@gmail.com>
2021-12-29 14:41:51 +0300

    :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className=" flex fixed justify-center items-center h-screen w-screen ">
+            <div className=" grid grid-cols-3 grid-rows-3  gap-3 h-100 w-100">
+                <div className="bg-red-200 h-20 w-20" />
+                <div className="bg-red-200 h-20 w-20" />
+                <div className="bg-red-200 h-20 w-20" />
+                <div className="bg-red-200 h-20 w-20" />
+                <div className="bg-red-200 h-20 w-20" />
+                <div className="bg-red-200 h-20 w-20" />
+                <div className="bg-red-200 h-20 w-20" />
+                <div className="bg-red-200 h-20 w-20" />
+                <div className="bg-red-200 h-20 w-20" />
+            </div>
+        </div>
+    );
 }
 
 export default App;
