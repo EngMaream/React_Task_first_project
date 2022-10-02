6784b4ca4575abab73621147cf32ecbfb2e64b2e
itsmaream <pro.maream.18@gmail.com>
2022-10-02 11:28:21 +0300

    asd

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
+import { useState } from "react";
 import "./index.css";
 import Star from "./Star";
+const IMAGES = ["im1", "im2", "im3", "im4", "im5", "im6", "im7"];
 function App() {
+    const [conter, setConter] = useState(0);
     return (
-        <div className="md:flex flex flex-col justify-center items-center h-screen w-screen ">
-            <Star />
-            <div
-                className=" grid grid-cols-2 grid-rows-3
-              gap-4 h-100 w-100 ">
-                <div className="bg-red-200 h-20 w-20 hover:w-40 col-span-2" />
-                <div className="bg-red-200 h-20 w-20 hover:w-40" />
-                <div className="bg-red-200 h-20 w-20 hover:w-40" />
-                <div className="bg-red-200 h-20 w-20 hover:w-40" />
-                <div className="bg-red-200 h-20 w-20 hover:w-40" />
-                <div className="bg-red-200 h-20 w-20 hover:w-40" />
-                <div className="bg-red-200 h-20 w-20 hover:w-40" />
-                <div className="bg-red-200 h-20 w-20 hover:w-40" />
-                <div className="bg-red-200 h-20 w-20 hover:w-40" />
-            </div>{" "}
-            <div className="bg-gradient-to-r from-indigo-500 via-purple-500  to-pink-500 w-full h-12"></div>
+        <div className="md:flex flex flex-col h-screen w-screen items-center justify-center">
+            <div className="flex flex-row h-60 relative ">
+                <p
+                    className="absolute bg-white rounded-full w-8 h-8 text-center text-blue-500 font-bold top-1/2 right-6 cursor-pointer z-10"
+                    onClick={() => {
+                        if (conter < IMAGES.length - 1) setConter(conter + 1);
+                        else setConter(0);
+                    }}>
+                    {">"}
+                </p>
+                <p
+                    className="absolute bg-white rounded-full w-8 h-8 text-center text-blue-500 font-bold top-1/2 left-6 cursor-pointer z-10"
+                    onClick={() => {
+                        if (conter > 0) setConter(conter - 1);
+                        else setConter(IMAGES.length - 1);
+                    }}>
+                    {"<"}
+                </p>
+                <img
+                    src={`${"/img/" + IMAGES[conter === 0 ? IMAGES.length - 1 : conter - 1] + ".jpg"}`}
+                    alt=""
+                    className="object-cover object-centerl h-60 w-40 filter blur-sm contrast-100"
+                />
+                <img src={`${"/img/" + IMAGES[conter] + ".jpg"}`} alt="" className="object-fill h-60 w-96" />
+                <img
+                    src={`${"/img/" + IMAGES[conter === IMAGES.length - 1 ? 0 : conter + 1] + ".jpg"}`}
+                    alt=""
+                    className="object-cover  object-center h-60 w-40 filter blur-sm contrast-100"
+                />
+            </div>
+            {/* 
+            <Star /> */}
         </div>
     );
 }
